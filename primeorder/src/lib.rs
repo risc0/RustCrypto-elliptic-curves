38f6b7233fb84f74a3f83b3f3b79f315af143421
Preston Ong <preston4896@me.com>
2025-03-06 18:18:49 +0800

    Risc0 FieldElement384 struct and P384 PrimeCurveParams trait for Risc0-bigint2

--- primeorder/src/lib.rs
+++ primeorder/src/lib.rs
@@ -20,6 +20,11 @@ mod dev;
 mod field;
 mod projective;
 
+/// WARNING: This is not part of the public API of the crate, and will not follow semver guarantees.
+#[cfg(all(target_os = "zkvm", target_arch = "riscv32"))]
+#[path = "risc0.rs"]
+pub mod __risc0;
+
 pub use crate::{affine::AffinePoint, projective::ProjectivePoint};
 pub use elliptic_curve::{
     self, generic_array, point::Double, Field, FieldBytes, PrimeCurve, PrimeField,
@@ -29,6 +34,7 @@ use elliptic_curve::CurveArithmetic;
 
 /// Parameters for elliptic curves of prime order which can be described by the
 /// short Weierstrass equation.
+#[cfg(not(all(target_os = "zkvm", target_arch = "riscv32")))]
 pub trait PrimeCurveParams:
     PrimeCurve
     + CurveArithmetic
@@ -50,4 +56,51 @@ pub trait PrimeCurveParams:
 
     /// Generator point's affine coordinates: (x, y).
     const GENERATOR: (Self::FieldElement, Self::FieldElement);
+}
+
+#[cfg(all(target_os = "zkvm", target_arch = "riscv32"))]
+use risc0_bigint2::{ec, field as Risc0Field};
+
+/// Parameters for elliptic curves of prime order which can be described by the
+/// short Weierstrass equation.
+#[cfg(all(target_os = "zkvm", target_arch = "riscv32"))]
+pub trait PrimeCurveParams:
+    // TODO this doesn't support different bit widths
+    ec::Curve<{Risc0Field::FIELD_384_WIDTH_WORDS}> + 
+    Sized +
+    PrimeCurve
+    + CurveArithmetic
+    + CurveArithmetic<AffinePoint = AffinePoint<Self>>
+    + CurveArithmetic<ProjectivePoint = ProjectivePoint<Self>>
+{
+    /// Base field element type.
+    type FieldElement: PrimeField<Repr = FieldBytes<Self>>;
+
+    /// [Point arithmetic](point_arithmetic) implementation, might be optimized for this specific curve
+    type PointArithmetic: point_arithmetic::PointArithmetic<Self>;
+
+    /// Coefficient `a` in the curve equation.
+    const EQUATION_A: Self::FieldElement;
+
+    /// Coefficient `b` in the curve equation.
+    const EQUATION_B: Self::FieldElement;
+
+    /// Generator point's affine coordinates: (x, y).
+    const GENERATOR: (Self::FieldElement, Self::FieldElement);
+
+    ///  Curve prime in little-endian words to be compatible with risc0 expected layout.
+    const PRIME_LE_WORDS: [u32; Risc0Field::FIELD_384_WIDTH_WORDS];
+
+    /// Order of the curve in little-endian words to be compatible with risc0 expected layout.
+    const ORDER_LE_WORDS: [u32; Risc0Field::FIELD_384_WIDTH_WORDS];
+
+    /// Coefficient `a` in the curve equation in little-endian words to be compatible with risc0
+    /// expected layout.
+    const EQUATION_A_LE: __risc0::FieldElement384<Self>;
+
+    /// Coefficient `b` in the curve equation in little-endian words to be compatible with risc0
+    /// expected layout.
+    const EQUATION_B_LE: __risc0::FieldElement384<Self>;
+
+    fn from_u32_words_le(words: [u32; Risc0Field::FIELD_384_WIDTH_WORDS]) -> Self::FieldElement;
 }
